f2738379bcb73f88da69d19ad52bec9b7dc9d763
Solaem Abdullah Fahim <solaemabdullahfahim@gmail.com>
2022-07-16 21:21:22 +0600

    added

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -4,42 +4,42 @@
  <div class="row justify-content-center">
   <div class="col-md-8">
     <div class="card">
-      <div class="card-header">{{__('Update Employee')}} </div>
+      <div class="card-header">{{('Update Employee')}} </div>
       <div class="card-body">
         <br><br><br>
-         
-        <form  action="{{url ('employee/update/{id}')}}" method="POST" enctype="multipart/form-data" >
-        @csrf
-        <input type="hidden" name="e_id" value="">
+        
+        <form method="POST"  action=" {{url ('/employee/edit')}} " >
         <div class="form-group">
+        {{@csrf_field()}}
+          <input type="hidden" name="e_id" value="">
           <label for="name">Employee Name</label>
-          <input type="text" class="form-control" name="name" aria-describedby="emailHelp" value = ""> <br><br>
+          <input type="text" class="form-control" name="e_name"  value = " "> <br>
           @error('name')
-         <span class="text-danger">{{$message}}</span>
+          <span class="text-danger">{{$message}}</span>
          @enderror
          <br><br>
           <label for="phone">Employee Phone</label>
-          <input type="text" class="form-control" name="phn" aria-describedby="emailHelp" value ="" > <br><br>
+          <input type="text" class="form-control" name="e_phn" value =" " > <br>
           @error('phone')
           <span class="text-danger">{{$message}}</span>
           @enderror
           <br><br>
           <label for="Email">Employee Email</label>
-          <input type="text" class="form-control" name="mail" aria-describedby="emailHelp" value = "" > <br><br>
+          <input type="text" class="form-control" name="e_mail"  value = "" > <br>
           @error('Email')
          <span class="text-danger">{{$message}}</span>
          @enderror
          <br><br>
 
           <label for="address">Employee Address</label>
-          <input type="text" class="form-control" name="add" aria-describedby="emailHelp" value = ""> <br><br>
+          <input type="text" class="form-control" name="e_add"  value = ""> <br>
           @error('address')
          <span class="text-danger">{{$message}}</span>
          @enderror
          <br><br>
           
         </div>
-         <button type="submit" class = "btn btn-primary"> Update </button>
+         <button  type="submit" class = "btn btn-primary"> Update </button> </a>
         </form>
       </div>
 
